db7f0674dc4c216d9096754b40398e767a9eb1b8
mykel68 <olowookeremicheal9568@gmail.com>
2025-01-28 15:49:26 +0100

    ci:added back delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,14 +89,14 @@ const PhoneChecker = () => {
             maxLength={11}
           />
 
-          {/* <button
+          <button
             type="submit"
             className="bg-red-700 p-2 text-white rounded-full transition-all ease-out duration-200 hover:shadow-lg"
             onClick={handleDelete}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
 
-          </button> */}
+          </button>
         </div>
 
         {/* Keypad */}
